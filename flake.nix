d3d24bd5d1f9e7c795ea66785a427899180b1ab1
gawakawa <hirakuhasegawa@gmail.com>
2024-05-26 19:38:29 +0900

    Add stable nixpkgs to inputs

--- flake.nix
+++ flake.nix
@@ -2,13 +2,14 @@
 	description = "Darwin system flake";
 
 	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+		nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+		nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-23.11-darwin";
 		nix-darwin = {
 			url = "github:LnL7/nix-darwin";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
 		home-manager = {
-			url = "github:nix-community/home-manager";
+			url = "github:nix-community/home-manager/release-23.11";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
 	};
