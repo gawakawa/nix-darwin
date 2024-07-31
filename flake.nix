a17ab1174e85456120dec2aecd6f31e60fe60e26
gawakawa <hirakuhasegawa@gmail.com>
2024-07-31 19:50:27 +0900

    Introduce spago@next

--- flake.nix
+++ flake.nix
@@ -12,12 +12,24 @@
 			url = "github:nix-community/home-manager/release-24.05";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
+		purescript-overlay = {
+			url = "github:thomashoneyman/purescript-overlay";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
 	};
 
-	outputs = inputs@{ self, nixpkgs, nix-darwin, home-manager, ... }:
+	outputs = inputs@{ self, nixpkgs, nix-darwin, home-manager,  purescript-overlay, ... }:
+	let 
+		overlays = {
+			purescript = purescript-overlay.overlays.default;
+		};
+	in
 	{
 		# Build darwin flake using:
 		# $ darwin-rebuild build --flake ".#mac" --impure
+
+		overlays = overlays;
+
 		darwinConfigurations."mac" = nix-darwin.lib.darwinSystem {
 			system = "aarch64-darwin";
 			modules = [ 
@@ -32,8 +44,19 @@
 						users.kawa = import /Users/kawa/.config/nix-darwin/home.nix; # HACK: 相対パスで済ませたい
 					};
 				}
+
+				({ pkgs, ... }: {
+					nixpkgs.overlays = builtins.attrValues overlays;
+					environment.systemPackages = with pkgs; [
+						purs
+						spago-unstable
+						purs-tidy-bin.purs-tidy-0_10_0
+						purs-backend-es
+					];
+				})
 			];
-			specialArgs = { inherit (inputs) self nixpkgs; };
+			
+			specialArgs = { inherit (inputs) self nixpkgs overlays; };
 		};
 
 		# Expose the package set, including overlays, for convenience.
