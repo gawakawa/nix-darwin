b100b72fd6239748b5f09e321d7ba780f42ee735
gawakawa <hirakuhasegawa@gmail.com>
2024-08-15 14:06:00 +0900

    Add WezTerm config

--- wezterm.lua
+++ wezterm.lua
@@ -1,6 +1,18 @@
 local wezterm = require 'wezterm'
 
 return {
-    font = wezterm.font("JetBrains Mono"),
+    color_scheme = 'tokyonight',
+
+    font = wezterm.font('Fira Code'),
     font_size = 15.0,
+
+    initial_cols = 100,
+    initial_rows = 30,
+
+    window_padding = {
+        left = 10,
+        right = 10,
+        top = 10,
+        bottom = 10,
+    },
 }
