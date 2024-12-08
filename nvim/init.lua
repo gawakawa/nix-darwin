a99e4cde1f6472ee21221178e11f193c74ac9ee1
gawakawa <hirakuhasegawa@gmail.com>
2024-12-09 00:11:52 +0900

    🔥 Remove keymap

--- nvim/init.lua
+++ nvim/init.lua
@@ -13,13 +13,6 @@ require('kawa/plugins/toggleterm')
 require('kawa/plugins/nvim_tree')
 require("telescope").load_extension "file_browser"
 
-vim.api.nvim_set_keymap(
-  "n",
-  "<space>ff",
-  ":Telescope file_browser<CR>",
-  { noremap = true }
-)
-
 require("telescope").setup{
   pickers = {
 		find_files = {
@@ -29,34 +22,3 @@ require("telescope").setup{
 }
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>p', builtin.find_files, {})
-vim.keymap.set('n', '<leader>f', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-
-vim.g.mapleader = ' ' -- 半角スペースを設定しています。
-vim.keymap.set('i', 'jj', '<ESC>', {silent=true}) -- インサートモードを抜ける
-vim.keymap.set('n', ';', ':', {silent=true}) -- シフトを押すことなく、;を入力する。割と便利
-
-vim.keymap.set('n', '<leader>sv', '<C-w>v') -- ウィンドウを垂直方向に分割する
-vim.keymap.set('n', '<leader>sh', '<C-w>s') -- ウィンドウを水平に分割する
-vim.keymap.set('n', '<leader>se', '<C-w>=') -- ウィンドウの幅を等分にする
-vim.keymap.set('n', '<leader>sx', ':close<CR>') -- 現在、カーソルがいるウィンドウを閉じる
-vim.keymap.set('n', '<leader>sw', '<C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>>') -- ウィンドウの幅を少し広げる
-vim.keymap.set('n', '<leader>sww', '<C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>>') -- ウィンドウの幅を大きく広げる
-vim.keymap.set('n', '<leader>st', '<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><') -- ウィンドウの幅を少し狭める
-vim.keymap.set('n', '<leader>stt', '<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><') -- ウィンドウの幅を大きく狭める
-
--- タブを作成、削除
-vim.keymap.set('n', '<leader>to', ':tabnew<CR>')
-vim.keymap.set('n', '<leader>tx', ':tabclose<CR>')
-vim.keymap.set('n', '<leader>tn', ':tabn<CR>')
-vim.keymap.set('n', '<leader>tp', ':tabp<CR>')
--- ウィンドウを移動する
-vim.keymap.set('n', '<C-l>', '<C-w>l')
-vim.keymap.set('n', '<C-h>', '<C-w>h')
-vim.keymap.set('n', '<C-j>', '<C-w>j')
-vim.keymap.set('n', '<C-k>', '<C-w>k')
--- 単語を' " どちらかで囲う
-vim.keymap.set('n', '<leader>aw', 'ciw""<Esc>P')
-vim.keymap.set('n', '<leader>aW', "ciw''<Esc>P")
--- nvim/lua/kawa/plugins/bufferline.lua
+++ nvim/lua/kawa/plugins/bufferline.lua
@@ -27,6 +27,3 @@ require'bufferline'.setup({
     }
   },
 })
-
-vim.keymap.set('n', '<Tab>', '<Cmd>BufferLineCycleNext<CR>', {})
-vim.keymap.set('n', '<S-Tab>', '<Cmd>BufferLineCyclePrev<CR>', {})
