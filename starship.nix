994ccfd6bb04d7c44aa91f7cd1fac21f9e28232b
gawakawa <hirakuhasegawa@gmail.com>
2025-02-02 10:00:49 +0900

    🧑‍💻 Edit prompt design

--- starship.nix
+++ starship.nix
@@ -1,16 +1,49 @@
 {
     programs.starship = {
         enable = true;
-        # Configuration written to ~/.config/starship.toml
         settings = {
             add_newline = true;
+
+            directory = {
+                truncation_length = 0;
+                truncate_to_repo = false;
+                home_symbol = "~";
+            };
+
+            time = {
+                disabled = false;
+                style = "bold yellow";
+                format = "🕐$time";
+                time_format = "%H:%M";
+            };
+
+            cmd_duration = {
+                style = "bold red";
+                format = "took [$duration](bold yellow) ";
+                min_time = 2000;
+            };
 
             character = {
                 success_symbol = "[λ](bold green)";
                 error_symbol = "[λ](bold red)";
             };
 
-        # package.disabled = true;
+            modules = [
+              "directory"
+              "git_branch"
+              "git_status"
+              "cmd_duration"
+              "line"
+              "time"
+              "character"
+            ];
+
+            format = "$directory$git_branch$git_status$cmd_duration$fill $time\n$character";
+
+            fill = {
+              symbol = "─";
+              style = "bold blue";
+            };
         };
     };
 }
