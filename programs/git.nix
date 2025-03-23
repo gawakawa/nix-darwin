08b63f6f342896d4eb7ac07f578eaf423cf9bb54
gawakawa <mashurakiryu@icloud.com>
2025-03-23 21:52:55 +0900

    :wrench: Add git config

--- programs/git.nix
+++ programs/git.nix
@@ -1,8 +1,13 @@
 {
   programs.git = {
     enable = true;
+    lfs.enable = true;
+
+    userName = "gawakawa";
+    userEmail = "mashurakiryu@icloud.com";
 
     extraConfig = {
+      init.defaultBranch = "main";
       core = {
         editor = "nvim";
       };
