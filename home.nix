6a8e46668f3677505763df0a2b68ae4b7912de4d
gawakawa <mashurakiryu@icloud.com>
2025-03-23 21:32:45 +0900

    :truck: Change directory structure of home manager

--- home.nix
+++ home.nix
@@ -1,24 +1,24 @@
 { config, lib, pkgs,  ... }:
 {
-    home = rec {
-        username = "iota";
-        homeDirectory = lib.mkForce "/Users/${username}";
-        enableNixpkgsReleaseCheck = true;
-        stateVersion = "24.11";
-        packages = with pkgs; [
-            curl
-            vscode
-            idris2
-        ];
-    };
-    programs.home-manager.enable = true;
-
-    imports = [
-        ./zsh.nix
-        ./direnv.nix
-	./git.nix
-        ./wezterm.nix
-        ./neovim.nix
-        ./starship.nix
+  home = rec {
+    username = "iota";
+    homeDirectory = lib.mkForce "/Users/${username}";
+    enableNixpkgsReleaseCheck = true;
+    stateVersion = "24.11";
+    packages = with pkgs; [
+      curl
+      vscode
+      idris2
     ];
+  };
+  programs.home-manager.enable = true;
+
+  imports = [
+    ./programs/zsh.nix
+    ./programs/direnv.nix
+    ./programs/git.nix
+    ./programs/wezterm.nix
+    ./programs/neovim.nix
+    ./programs/starship.nix
+  ];
 }
