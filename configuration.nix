147e84c77fd8f1e89aae177a1adf013f82d03d19
gawakawa <mashurakiryu@icloud.com>
2025-04-01 00:51:47 +0900

    :heavy_plus_sign: Add fira-code nerd fonts

--- configuration.nix
+++ configuration.nix
@@ -1,90 +1,91 @@
 { pkgs, self, nixpkgs, ... }:
 {
-    # List packages installed in system profile. To search by name, run:
-    # $ nix-env -qaP | grep wget
-    environment.systemPackages = with pkgs; [
-        awscli2
-        colima
-        claude-code
-        direnv
-        discord
-        docker
-        docker-compose
-        docker-credential-helpers
-        fd
-        gh
-        gitmoji-cli
-        google-chrome
-        google-cloud-sdk
-        httpie
-        neofetch
-        qemu
-        rlwrap
-        slack
-        starship
-        tree
-        utm
-        zoom-us
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [
+    awscli2
+    colima
+    claude-code
+    direnv
+    discord
+    docker
+    docker-compose
+    docker-credential-helpers
+    fd
+    gh
+    gitmoji-cli
+    google-chrome
+    google-cloud-sdk
+    httpie
+    neofetch
+    qemu
+    rlwrap
+    slack
+    starship
+    tree
+    utm
+    zoom-us
+  ];
+
+  homebrew = {
+    enable = true;
+    onActivation.cleanup = "uninstall";
+
+    taps = [];
+    brews = [
+      "gnu-time"
+      "mas"
     ];
-
-    homebrew = {
-        enable = true;
-        onActivation.cleanup = "uninstall";
-
-        taps = [];
-        brews = [
-            "gnu-time"
-            "mas"
-        ];
-        casks = [
-            "claude"
-            "notion"
-            "visual-studio-code"
-            "wezterm"
-        ];
-        masApps = {
-            "LINE" = 539883307;
-            "Goodnotes 6" = 1444383602;
-            "Kindle" = 302584613;
-        };
+    casks = [
+      "claude"
+      "notion"
+      "visual-studio-code"
+      "wezterm"
+    ];
+    masApps = {
+      "LINE" = 539883307;
+      "Goodnotes 6" = 1444383602;
+      "Kindle" = 302584613;
     };
+  };
 
-    fonts = {
-        packages = with pkgs; [
-            fira-code
-            fira-code-symbols
-        ];
+  fonts = {
+    packages = with pkgs; [
+      fira-code
+      fira-code-symbols
+      nerd-fonts.fira-code
+    ];
+  };
+
+  # Necessary for using flakes on this system.
+  nix = {
+    enable = false;
+    package = pkgs.nix;
+    settings = {
+      experimental-features = "nix-command flakes";
     };
+  };
 
-    # Necessary for using flakes on this system.
-    nix = {
-        enable = false;
-        package = pkgs.nix;
-        settings = {
-            experimental-features = "nix-command flakes";
-        };
-    };
+  # Create /etc/zshrc that loads the nix-darwin environment.
+  programs.zsh.enable = true;
 
-    # Create /etc/zshrc that loads the nix-darwin environment.
-    programs.zsh.enable = true;
+  system = {
+    # Set Git commit hash for darwin-version.
+    configurationRevision = self.rev or self.dirtyRev or null;
 
-    system = {
-        # Set Git commit hash for darwin-version.
-        configurationRevision = self.rev or self.dirtyRev or null;
+    # Used for backwards compatibility, please read the changelog before changing.
+    # $ darwin-rebuild changelog
+    stateVersion = 4;
+  };
 
-        # Used for backwards compatibility, please read the changelog before changing.
-        # $ darwin-rebuild changelog
-        stateVersion = 4;
-    };
+  nixpkgs = {
+    config.allowUnfree = true;
+    hostPlatform = "aarch64-darwin";
+  };
 
-    nixpkgs = {
-        config.allowUnfree = true;
-        hostPlatform = "aarch64-darwin";
-    };
-
-    # Define user
-    users.users.iota = {
-        name = "iota";
-        home = "/Users/iota";
-    };
+  # Define user
+  users.users.iota = {
+    name = "iota";
+    home = "/Users/iota";
+  };
 }
