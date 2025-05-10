f10fb31403e143b6352d999f6b607ae40a03ee37
gawakawa <mashurakiryu@icloud.com>
2025-05-11 02:25:48 +0900

    :recycle: Refactor conform config

--- programs/nvim/lua/plugins/conform.lua
+++ programs/nvim/lua/plugins/conform.lua
@@ -1,14 +1,24 @@
 return {
-	{
-		"stevearc/conform.nvim",
-		opts = {
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_format = "fallback",
-			},
-			formatters_by_ft = {
-				lua = { "stylua" },
-			},
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			"<leader>f",
+			function()
+				require("conform").format({ async = true })
+			end,
+			mode = "",
+			desc = "Format buffer",
 		},
+	},
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+		},
+		default_format_opts = {
+			lsp_format = "fallback",
+		},
+		format_on_save = { timeout_ms = 500 },
 	},
 }
