68b1dbb2376f4f54dcf20c38695d042a31fa076c
gawakawa <mashurakiryu@icloud.com>
2025-05-10 22:22:46 +0900

    :technologist: Brighten colors for comments and unused variables

--- programs/nvim/lua/plugins/tokyonight.lua
+++ programs/nvim/lua/plugins/tokyonight.lua
@@ -6,6 +6,12 @@ return {
     require("tokyonight").setup({
       transparent = true,
       style = "night",
+      on_colors = function(colors)
+        colors.comment = "#7a88cf"
+      end,
+      on_highlights = function(highlights, colors)
+        highlights.DiagnosticUnnecessary = { fg = "#7a88cf" }
+      end,
     })
     vim.cmd([[colorscheme tokyonight]])
   end,
