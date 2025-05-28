f9beab9769178dc813b31f206193397ed15921d0
gawakawa <mashurakiryu@icloud.com>
2025-05-28 22:15:59 +0900

    :lipstick: Make toggleterm background transparent

--- programs/nvim/lua/plugins/toggleterm.lua
+++ programs/nvim/lua/plugins/toggleterm.lua
@@ -1,17 +1,30 @@
 return {
-  "akinsho/toggleterm.nvim",
-  version = "*",
-  config = function()
-    require("toggleterm").setup{
-      open_mapping = [[<c-t>]],
-      start_in_insert = true,
-    }
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	config = function()
+		require("toggleterm").setup({
+			open_mapping = [[<c-t>]],
+			start_in_insert = true,
+			highlights = {
+				Normal = {
+					guibg = "NONE",
+					ctermbg = "NONE",
+				},
+				NormalFloat = {
+					guibg = "NONE",
+					ctermbg = "NONE",
+				},
+				FloatBorder = {
+					guibg = "NONE",
+					ctermbg = "NONE",
+				},
+			},
+		})
 
-    vim.keymap.set("n", "<c-1>", ":1ToggleTerm<CR>")
-    vim.keymap.set("n", "<c-2>", ":2ToggleTerm<CR>")
-    vim.keymap.set("n", "<c-3>", ":3ToggleTerm<CR>")
-    vim.keymap.set("n", "<c-4>", ":4ToggleTerm<CR>")
-    vim.keymap.set("n", "<c-5>", ":5ToggleTerm<CR>")
-  end,
+		vim.keymap.set("n", "<c-1>", ":1ToggleTerm<CR>")
+		vim.keymap.set("n", "<c-2>", ":2ToggleTerm<CR>")
+		vim.keymap.set("n", "<c-3>", ":3ToggleTerm<CR>")
+		vim.keymap.set("n", "<c-4>", ":4ToggleTerm<CR>")
+		vim.keymap.set("n", "<c-5>", ":5ToggleTerm<CR>")
+	end,
 }
-
